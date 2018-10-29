9871e625755b99b2904d28a8b44096e34a2d4406
jake <jake@globebyte.com>
2018-10-29 14:38:05 +0000

    Amend to Not Found message

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -15,8 +15,8 @@ export default () => {
     return (
         <div className="boxed-view">
             <div className="boxed-view__box">
-                <h1>Page Not Found</h1>
-                <p>We're unable tofind that page.</p>
+                <h1>Oh how embarrassing</h1>
+                <p>We're unable to find that page.</p>
                 <Link className="button button--link" to="/">Head Home</Link>
             </div>
         </div>
